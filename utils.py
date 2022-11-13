5c79e2ed808ff09365b230c2b9dc76fbbe3f06a9
hewtungyuen <hewtungyuen@gmail.com>
2022-11-13 15:01:49 +0800

    update utils

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+from imblearn.combine import SMOTEENN
 from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score, fbeta_score, roc_auc_score, \
     average_precision_score
+
+RANDOM_STATE = 20
 
 
 def get_train_and_test_values(df_train, df_valid, df_test):
@@ -13,6 +16,15 @@ def get_train_and_test_values(df_train, df_valid, df_test):
     y_test = df_test['FraudFound_P'].values
 
     return X_train, y_train, X_valid, y_valid, X_test, y_test
+
+
+def with_resampling(df_train, df_valid, df_test):
+    X_train, y_train, X_valid, y_valid, X_test, y_test = get_train_and_test_values(df_train, df_valid, df_test)
+
+    smote_enn = SMOTEENN(random_state=RANDOM_STATE, sampling_strategy=0.6)
+    X_train_resampled, y_train_resampled = smote_enn.fit_resample(X_train, y_train)
+
+    return X_train_resampled, y_train_resampled, X_valid, y_valid, X_test, y_test
 
 
 def obtain_predictions(model, X_train, y_train, X_test, y_test):
